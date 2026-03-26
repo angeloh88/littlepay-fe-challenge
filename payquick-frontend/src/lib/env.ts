886e242a883f9ba02fb9266c5eb831e9dec6a7de
Angelo Hernandez <angeloh.design@gmail.com>
2026-03-26 13:45:56 +0800

    Centralizing env access

---
+++ payquick-frontend/src/lib/env.ts
@@ -0,0 +1,5 @@
+export const env = {
+    MOCK_API_ORIGIN: process.env.MOCK_API_ORIGIN || "http://localhost:3001",
+    NEXT_PUBLIC_APP_URL:
+        process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
+};
